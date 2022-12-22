923b32cfc337705758b469ef9e0399782d5cd14d
Brandon Vasquez <thatsjaskett@gmail.com>
2022-12-22 18:27:38 +0100

    Aggunta colore esito nello Script

--- Gioco_dei_dadi/js/scripts.js
+++ Gioco_dei_dadi/js/scripts.js
@@ -8,9 +8,12 @@ btn.addEventListener('click',
         let user_try = Math.ceil(Math.random() * 5) + 1;
 
         user_points.innerHTML = `Il punteggio dell'utente è ${user_try}`;
-        pc_points.innerHTML = `Il punteggio dell'utente è ${pc_try}`;
+        user_points.classList = 'pad-y';
+        pc_points.innerHTML = `Il punteggio del PC é ${pc_try}`;
+        pc_points.classList = 'pad-y';
 
         let result;
+
         if(pc_try > user_try) {
             result = "Hai perso!";
         } else if (user_try > pc_try) {
@@ -22,9 +25,17 @@ btn.addEventListener('click',
         let risultato = document.getElementById('risultato');
         risultato.innerHTML = result;
 
-        risultato.style.backgroundColor = "LightGrey";
-        risultato.style.fontSize = "20px";
-        risultato.style.fontWeight = "bold";
-        console.log(result)
+        risultato.style.display = 'block';
+        risultato.className = 'risultato';
+
+        if(result == 'Hai vinto!') {
+            risultato.style.backgroundColor = 'LightGreen';
+        } else if(result == 'Hai perso!') {
+            risultato.style.backgroundColor = "Red";
+        } else {
+            risultato.style.backgroundColor = "LightGrey";
+        }
+
+        console.log(result);
     }
 );
