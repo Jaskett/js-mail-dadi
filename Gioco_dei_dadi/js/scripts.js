18a2a89a5fdb236f11f7a626de5655cac875c1bd
Brandon Vasquez <thatsjaskett@gmail.com>
2022-12-22 17:13:59 +0100

    Creazione gioco dei dadi

---
+++ Gioco_dei_dadi/js/scripts.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById('sub');
+let user_points = document.getElementById('user_points');
+let pc_points = document.getElementById('pc_points');
+
+btn.addEventListener('click',
+    function() {
+        let pc_try = Math.ceil(Math.random() * 5) + 1;
+        let user_try = Math.ceil(Math.random() * 5) + 1;
+
+        user_points.innerHTML = `Il punteggio dell'utente è ${user_try}`;
+        pc_points.innerHTML = `Il punteggio dell'utente è ${pc_try}`;
+
+        let result;
+        if(pc_try > user_try) {
+            result = "Hai perso!";
+        } else if (user_try > pc_try) {
+            result = "Hai vinto!";
+        } else {
+            result = "Pareggio!";
+        }
+
+        let risultato = document.getElementById('risultato');
+        risultato.innerHTML = result;
+
+        risultato.style.backgroundColor = "LightGrey";
+        risultato.style.fontSize = "20px";
+        risultato.style.fontWeight = "bold";
+        console.log(result)
+    }
+);
